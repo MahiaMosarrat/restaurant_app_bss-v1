d52b4cca924b8b81854a172395780c992505b742
Mahia Mosarrat <mahiarite@gmail.com>
2026-02-02 12:22:55 +0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/restaurant_app_bss-v1",
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+       
       },
     }),
   ],
